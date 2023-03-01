ec519100925a94c5dc3eccc860ad52bb1e67e92c
Albert <user@WINDOWS-O7N9M38>
2023-03-01 10:40:46 +0400

    add new component

---
+++ src/app/klinik/klinik.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KlinikComponent } from './klinik.component';
+
+describe('KlinikComponent', () => {
+  let component: KlinikComponent;
+  let fixture: ComponentFixture<KlinikComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ KlinikComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(KlinikComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/klinik/klinik.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-klinik',
+  templateUrl: './klinik.component.html',
+  styleUrls: ['./klinik.component.css']
+})
+export class KlinikComponent {
+
+}
